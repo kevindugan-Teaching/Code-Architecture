a4531348fd679e16237c8aa264063328a1bd202c
Kevin J. Dugan <dr.kevin.dugan@gmail.com>
2021-12-20 15:33:29 -0500

    Updating Vector implementation

--- Solutions/Python/LinearAlgebra/Vector/Vector.py
+++ Solutions/Python/LinearAlgebra/Vector/Vector.py
@@ -1,11 +1,27 @@
-from AbstractVector import AbstractVector
+from .AbstractVector import AbstractVector
+import numpy as np
 
 class Vector(AbstractVector):
 
     def __init__(self, values=None, size=None):
-        pass
-        # assert values is None or size is None, "Both values and size specified, specify only one"
-        # if size is not None:
-        #     self.values = [0.0]*size
-        # elif values is not None:
-        #     self.values = [i for i in values]
+        # Default: Empty Vector
+        if size is None and values is None:
+            self.values = []
+            self.size = len(self.values)
+
+        # Both keys cannot be specified
+        assert values is None or size is None, "Both values and size specified, specify only one"
+
+        # Construct
+        if size is not None:
+            self.values = [0.0]*size
+            self.size = len(self.values)
+        elif values is not None:
+            self.values = [i for i in values]
+            self.size = len(self.values)
+
+    def l2norm(self):
+        result = 0.0
+        for it in self.values:
+            result += it*it
+        return np.sqrt(result)
--- Solutions/Python/LinearAlgebra/Vector/tests/test_standardVector.py
+++ Solutions/Python/LinearAlgebra/Vector/tests/test_standardVector.py
@@ -6,3 +6,21 @@ def test_constructor():
     a = Vector()
     assert a.size == 0
     assert a.values == []
+
+    # Test zero vector
+    b = Vector(size=3)
+    assert b.size == 3
+    assert b.values == pytest.approx([0, 0, 0])
+
+    # Test values vector
+    c = Vector(values=[-1, 2.3, 4.5, -4])
+    assert c.size == 4
+    assert c.values == pytest.approx([-1, 2.3, 4.5, -4])
+
+    # Check error condition
+    with pytest.raises(Exception):
+        d = Vector(size=3, values=[1, 2, 3])
+
+def test_l2norm():
+    a = Vector(values=[-1, 2, 1, -3, 1])
+    assert a.l2norm() == pytest.approx(4)
